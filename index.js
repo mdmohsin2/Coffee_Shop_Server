1e4ad6bd1b36d27b27c4fdd50605f9ff3d6e9818
mdmohsin2 <mohsinbd862554@gmail.com>
2023-05-14 00:35:32 +0600

    update data

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_pass}@cluster0.kjb9ctc.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kjb9ctc.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -20,6 +20,7 @@ const client = new MongoClient(uri, {
       deprecationErrors: true,
     }
   });
+
 
 
 async function run(){
@@ -44,5 +45,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, (req, res) => {
-    console.log(`assignment-11 server runnint on  ${port}`);
+    console.log(`server runnint on  ${port}`);
 })
